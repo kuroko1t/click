0046f41c8f271c5a7e2b22213e50893d8d97434c
kuroko1t <kurosawa.yk@gmail.com>
2020-09-30 14:27:14 +0900

    update

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	for {
-		robotgo.MouseClick("left", true)
+		//robotgo.MouseClick("left", true)
+		robotgo.MoveMouseSmooth(100, 200, 1.0, 100.0)
+		robotgo.ScrollMouse(10, "up")
 		time.Sleep(time.Second * 10)
 	}
 }
